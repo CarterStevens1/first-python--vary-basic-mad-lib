01f62a156e11c533c4b5227adb335db340258568
CarterStevens1 <CarterStevens1@users.noreply.github.com>
2021-06-10 11:38:56 +0100

    first commit

---
+++ mad-lib.py
@@ -0,0 +1,18 @@
+wordone = input( "first Word: ")
+word2 = input( "second Word: ")
+word3 = input( "third Word: ")
+word4 = input("fourth word: ")
+word5 = input("fifth word: ")
+word6 =input("sixth word: ")
+word7 =input("seventh word: ")
+
+print("It was during the battle of" + wordone +  "when I was running through a Pear when a Wolf went off right next to my platoon.")
+print("Our Slaughterer yelled for us to Integrate to the nearest" + word2 +  " we could find.")
+print("Our Slaughterer yelled for us to Integrate to the nearest" + word3 +  "we could find.")
+print("When we got to the" + word4 +  "we Clung to start a fire.")
+print("As we were starting the fire the enemy saw the Index from the fire and started" + word5 +  "at us.")
+print(" we all quickly ducked behind the Cold at the" + word6 +  "and returned fire.")
+print("we quickly eliminated the enemy and were" + word7+  "that we had won the battle.")
+
+
+    
